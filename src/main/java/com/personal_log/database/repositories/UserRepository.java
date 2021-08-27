42051618296eaf025584a6f5db7dea17e2d99243
Jack Clarke <jackfulcher09@gmail.com>
2021-08-27 12:36:09 -0400

    set up dev mongodb

--- src/main/java/com/personal_log/database/repositories/UserRepository.java
+++ src/main/java/com/personal_log/database/repositories/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends MongoRepository<User, String> {
+
 }
--- src/test/java/com/personal_log/database/repositories/UserRepositoryTest.java
+++ src/test/java/com/personal_log/database/repositories/UserRepositoryTest.java
@@ -3,13 +3,23 @@ package com.personal_log.database.repositories;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.personal_log.database.entities.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.io.UnsupportedEncodingException;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.not;
+import static org.hamcrest.Matchers.emptyString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,20 +27,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserRepositoryTest {
+    final Gson gson = new Gson();
+    final User test_user = new User("George M");
 
     @Autowired
     private MockMvc mvc;
 
+    private JsonObject getResponseBodyAsJson(MvcResult res) throws UnsupportedEncodingException {
+        String content = res.getResponse().getContentAsString();
+        assert content.length() > 0 : "res has no response body";
+        return (new JsonParser())
+                .parse(res
+                        .getResponse()
+                        .getContentAsString())
+                .getAsJsonObject();
+    }
+
     @Test
-    public void getUsers() throws Exception {
+    public void getUsersReturnsOK() throws Exception {
         MvcResult result = this.mvc
                 .perform(get("/users"))
                 .andExpect(status().isOk())
                 .andReturn();
-        String output = result.getResponse().getContentAsString();
-        // will be used in next test
-        // here as note
-        JsonObject data = new JsonParser().parse(output).getAsJsonObject();
+        JsonObject data = getResponseBodyAsJson(result);
+    }
+
+    @Test
+    public void postUsersReturnsOk() throws Exception {
+        final var postData = gson.toJson(test_user);
+        MvcResult result = this.mvc
+                .perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postData))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        JsonObject data = getResponseBodyAsJson(result);
+        assertTrue(data.get("id").getAsString().length() > 0);
 
     }
 
